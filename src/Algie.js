bf53db4d51a1de78199faff6e28cba4d9c870443
Maxime Devallez <maxdeval02@gmail.com>
2019-03-11 16:02:18 +0100

    done

--- src/Algie.js
+++ src/Algie.js
@@ -4,7 +4,19 @@ function algie_42() {
 };
 
 function algie_insert_everywhere(string, stringToInsert) {
-  // body...
+  if(_.isString(string) && _.isString(stringToInsert)) {
+    var result = [];
+    stringIntoArray = _.toArray(string);
+    _.each(stringIntoArray, function(value, key) {
+      //We add the string for each value of the array
+      result[key] = stringToInsert + stringIntoArray[key];
+    })
+    result[result.length] = stringToInsert;
+    return _.toString(_.join(result, ''));
+  }
+  else {
+    return _.toString(string);
+  }
 }
 
 
