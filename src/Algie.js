b6a3b4ff26829b967415b29f6a5eb19f48a58fd0
Maxime Devallez <maxdeval02@gmail.com>
2019-03-11 16:42:30 +0100

    trying stuff

--- src/Algie.js
+++ src/Algie.js
@@ -4,6 +4,24 @@ function algie_42() {
 };
 
 function algie_few_of(array) {
-  // body...
+  var counter = 0;
+  _.each(array), function(value){
+    if(value === true) {
+      counter = counter + 1;
+    }
+  }
+  console.log(counter)
+  if(counter === array.length){
+    return false;
+  }
+  if (_.includes(array, true)) {
+    return true
+  }
+  else {
+    return true;
+  }
 }
+
+
+algie_few_of([true, true, true])
 
