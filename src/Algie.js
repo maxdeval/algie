2053e59f7035beb228791e42fc75176eaf5e697e
Maxime Devallez <maxdeval02@gmail.com>
2019-03-11 13:09:48 +0100

    doesn't work with truthy values

--- src/Algie.js
+++ src/Algie.js
@@ -3,7 +3,18 @@ function algie_42() {
   return 42;
 };
 
-function algie_none_of(array) {
+var falsyValues = [false, null, undefined, 0, NaN, '']
+var truthyValues = ['0', 'false', [], {}]
 
+function algie_none_of(array) {
+  if (!(_.includes(array, true))) {
+    return true 
+  }
+  else if (_.includes(array, true)) {
+    return false
+  }
+/*  else if (_.includes(array, falsyValues)) {
+    return true
+  }*/
 }
 
