3d0d8795395d1a9cb15100477c41b95089c07ea5
Maxime Devallez <maxdeval02@gmail.com>
2019-03-11 14:02:11 +0100

    done

--- src/Algie.js
+++ src/Algie.js
@@ -4,7 +4,16 @@ function algie_42() {
 };
 
 function algie_multiply_each(array1, array2) {
-
+  if( _.isArray(array1) && _.isArray(array2) && array1.length === array2.length) { 
+    var result = [];
+    _.each(array1, function(value, key) {
+      result[key] = value * array2[key]; 
+    })
+    return result;
+  }
+  else {
+    return false
+  }
 }
 
 
