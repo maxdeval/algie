1cddc3effc174c43e012dd0ad373aec638c853b0
Maxime Devallez <maxdeval02@gmail.com>
2019-03-12 14:22:41 +0100

    init algie_is_decreasing

--- src/Algie.js
+++ src/Algie.js
@@ -3,12 +3,6 @@ function algie_42() {
   return 42;
 };
 
-function algie_allMultiplyBy(array, number) {
-  // Write function here
+function algie_is_decreasing(array) {
+  // body...
 }
-
-function algie_generateWord() {
-  // return "zaropi"
-}
-
-
