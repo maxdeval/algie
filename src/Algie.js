e345151da6e9bb566248a41d4a532bd5d942c1e9
Maxime Devallez <maxdeval02@gmail.com>
2019-03-12 13:17:38 +0100

    done

--- src/Algie.js
+++ src/Algie.js
@@ -4,7 +4,25 @@ function algie_42() {
 };
 
 function algie_largest_string(array) {
-  // body...
+  if(_.isArray(array)){
+    var length = 0;
+    var largestString = "";
+    _.each(array, function(value , key) {
+      if(_.isString(value)){
+        if(array[key].length > length) {
+          length = array[key].length;
+          largestString = array[key];
+        }       
+      }
+      else {
+        throw "A value in the array is not a string";
+      }
+    })
+    return largestString;    
+  }
+  else {
+    return false;
+  }
 }
 
 
