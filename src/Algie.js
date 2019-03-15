bc0c918b6fb66fd81ef1c50e9fe91f1b74303a08
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 11:03:47 +0100

    only work for 2 word

--- src/Algie.js
+++ src/Algie.js
@@ -4,7 +4,14 @@ function algie_42() {
 };
 
 function algie_initials(string) {
-  // body...
+  var names = string.split(' '),
+      initials = names[0].substring(0, 1).toUpperCase();
+  
+  if (names.length > 1) {
+      initials += names[names.length - 1].substring(0, 1).toUpperCase();
+  }
+  return initials;
 }
 
+algie_initials("One great journey")
 
