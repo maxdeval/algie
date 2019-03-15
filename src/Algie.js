64a4f4b49616b3d2869a3988c7209c0d72eaaf3a
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 12:29:27 +0100

    done

--- src/Algie.js
+++ src/Algie.js
@@ -5,10 +5,19 @@ function algie_42() {
 
 function algie_print_to_zero(number) {
   var result=[];
-  for(var i = 0; i < number; i++) {
-    result[i] = i
+  if(number >= 0){
+    for(var i = 0; i < number; i++) {
+      result[i] = i
+    }
+    return _.toInteger(_.join(_.reverse(result), ''))
   }
-  return _.toInteger(_.join(_.reverse(result), ''))
+  else if(number < 0){
+    for(var i = number; i < 1; i++) {
+      result[-i] = i;
+      console.log(result[i])
+    }
+    result.pop()
+    return (_.join(_.reverse(result), ''))  }
 }
 
-algie_print_to_zero(5)
+algie_print_to_zero(-12)
