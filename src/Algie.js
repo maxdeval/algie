1c2722d37e24df78543c4ec9bedca626baf0caa3
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 13:20:58 +0100

    done

--- src/Algie.js
+++ src/Algie.js
@@ -4,27 +4,26 @@ function algie_42() {
 };
 
 function algie_find_multiplicators(number) {
-  if (number < 1)
-    throw "Argument error";
-  var small = [];
-  var large = [];
-  var end = Math.floor(Math.sqrt(number));
-  for (var i = 1; i <= end; i++) {
-    if (number % i == 0) {
-      small.push(i);
-      if (i * i != number)  // Don't include a square root twice
-        large.push(number / i);
+  if(_.isInteger(number)){
+    if (number < 1)
+      throw "Argument error";
+    var small = [];
+    var large = [];
+    var end = Math.floor(Math.sqrt(number));
+    for (var i = 1; i <= end; i++) {
+      if (number % i == 0) {
+        small.push(i);
+        if (i * i != number)
+          large.push(number / i);
+      }
     }
+    large.reverse();
+    small = small.concat(large);
+    small.pop()
+    small.shift()
+    return _.reverse(small)
   }
-  large.reverse();
-  small = small.concat(large);
-  small.pop()
-  small.shift()
-  return _.reverse(small)
-/*  small.pop();
-  small.shift();
-  console.log(small)
-  console.log(large)*/
+  else {
+    return false;
+  }
 }
-
-algie_find_multiplicators(49)
