4c4d0262d547ff58fec4608bb3c1f325a252fa1c
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 10:38:35 +0100

    done

--- src/Algie.js
+++ src/Algie.js
@@ -6,7 +6,10 @@ function algie_42() {
 function algie_last_will_be_first(array) {
   var temp1 = _.last(array)
   var temp2 = _.first(array)
-  _.last(array) = temp2
-  _.first(array) = temp1
+  array[array.length - 1] = temp2
+  array[0] = temp1
+  return array
   console.log(array)
 }
+
+algie_last_will_be_first([12, 45, 8, 22, 89])
