ea7fc62e5a6e495f44b3cd557081bbca4ee821d0
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 12:30:43 +0100

    added conditions

--- src/Algie.js
+++ src/Algie.js
@@ -4,20 +4,25 @@ function algie_42() {
 };
 
 function algie_print_to_zero(number) {
-  var result=[];
-  if(number >= 0){
-    for(var i = 0; i < number; i++) {
-      result[i] = i
+  if(_.isInteger(number)){
+    var result=[];
+    if(number >= 0){
+      for(var i = 0; i < number; i++) {
+        result[i] = i
+      }
+      return _.toInteger(_.join(_.reverse(result), ''))
     }
-    return _.toInteger(_.join(_.reverse(result), ''))
+    else if(number < 0){
+      for(var i = number; i < 1; i++) {
+        result[-i] = i;
+      }
+      result.pop()
+      return _.join(_.reverse(result), '')  
+    }
   }
-  else if(number < 0){
-    for(var i = number; i < 1; i++) {
-      result[-i] = i;
-      console.log(result[i])
-    }
-    result.pop()
-    return (_.join(_.reverse(result), ''))  }
+  else {
+    return false
+  }
 }
 
 algie_print_to_zero(-12)
