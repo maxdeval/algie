99e8f594fc756fcdeeaa4b3264e87d03a3130597
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 14:27:54 +0100

    only 2 specs pass

--- src/Algie.js
+++ src/Algie.js
@@ -3,7 +3,20 @@ function algie_42() {
   return 42;
 };
 
-function algie_nice_draw(argument) {
-  // body...
+function algie_nice_draw(string1, string2, number) {
+  var i = 0;
+  var result = [];
+  while (i < number) {
+    i++
+    if(i < number){
+      result.push(string1)
+      if(i < number){
+        i++
+        console.log(i)
+        result.push(string2)}
+    }
+  }
+  return _.join(result, '')
 }
 
+algie_nice_draw("o", "a", 3)
