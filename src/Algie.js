fa14777ba742c771a649280b2977c698a0b39b89
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 15:30:34 +0100

    doesn't work

--- src/Algie.js
+++ src/Algie.js
@@ -4,5 +4,15 @@ function algie_42() {
 };
 
 function algie_closest_of2000(array) {
-  // body...
+  var myDate = new Date('January 1, 2000')
+  var closest = Infinity;
+  array.forEach(function(d) {
+   var date = new Date(array);
+   if (date >= myDate && date < closest) {
+      closest = d;
+   }
+  })
+  return closest
 }
+
+algie_closest_of2000([new Date('March 13, 2006 03:24:00'), new Date('June 13, 1995 03:24:00'), new Date('December 8, 2012 03:24:00'), new Date('May 3, 2019 03:24:00')])
