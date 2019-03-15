7f54c59c1089c5afb8358beed56ad8ae913e9950
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 16:58:49 +0100

    cancel modif

--- src/Algie.js
+++ src/Algie.js
@@ -3,14 +3,4 @@ function algie_42() {
   return 42;
 };
 
-function algie_max_of_consecutive_in_array(array) {
-  var counter = 0;
-  _.each(array, function(value, key){
-    if(value === value[key - 1]){
-      counter += 1
-    }
-  })
-  console.log(counter)
-}
 
-algie_max_of_consecutive_in_array(["a", "a", "c", "c", "c", "z"])
