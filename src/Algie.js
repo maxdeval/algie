987375f035e864033aa47eef756e9b8b8bdf0547
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 12:59:47 +0100

    done

--- src/Algie.js
+++ src/Algie.js
@@ -4,6 +4,27 @@ function algie_42() {
 };
 
 function algie_find_multiplicators(number) {
-  // body...
+  if (number < 1)
+    throw "Argument error";
+  var small = [];
+  var large = [];
+  var end = Math.floor(Math.sqrt(number));
+  for (var i = 1; i <= end; i++) {
+    if (number % i == 0) {
+      small.push(i);
+      if (i * i != number)  // Don't include a square root twice
+        large.push(number / i);
+    }
+  }
+  large.reverse();
+  small = small.concat(large);
+  small.pop()
+  small.shift()
+  return _.reverse(small)
+/*  small.pop();
+  small.shift();
+  console.log(small)
+  console.log(large)*/
 }
 
+algie_find_multiplicators(49)
