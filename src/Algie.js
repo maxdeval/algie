f993eeec10a386400d23e20f6c39c1f887a9f756
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 14:37:17 +0100

    done

--- src/Algie.js
+++ src/Algie.js
@@ -4,6 +4,19 @@ function algie_42() {
 };
 
 function algie_foo_bar_qix_zzz(number) {
-  // body...
+  if (number != 0  && number != null  && number % 15 === 0) {
+    return "qix"
+  }
+  else if (number != 0 && number != null  && number % 3 === 0) {
+    return "foo"
+  }
+  else if (number != 0 && number != null  && number % 5 === 0) {
+    return "bar"
+  }
+  else {
+    return "zzz"
+  }
 }
+
+algie_foo_bar_qix_zzz(null)
 
