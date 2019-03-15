cee9dc4c70da44498e106697b7d7bed09dac6025
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 16:11:29 +0100

    init algie_add2_dates

--- src/Algie.js
+++ src/Algie.js
@@ -3,12 +3,7 @@ function algie_42() {
   return 42;
 };
 
-function algie_allMultiplyBy(array, number) {
-  // Write function here
+function algie_add2_dates(date1, date2) {
+  // body...
 }
-
-function algie_generateWord() {
-  // return "zaropi"
-}
-
 
