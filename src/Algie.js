a7d8a98bb8a210bcc4ecf31da36688e1febf3609
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 15:03:59 +0100

    init algie_closest_of2000

--- src/Algie.js
+++ src/Algie.js
@@ -3,12 +3,6 @@ function algie_42() {
   return 42;
 };
 
-function algie_allMultiplyBy(array, number) {
-  // Write function here
+function algie_closest_of2000(array) {
+  // body...
 }
-
-function algie_generateWord() {
-  // return "zaropi"
-}
-
-
