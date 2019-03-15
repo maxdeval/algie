d3381082a58e9acbd02a2946fd21a437f95b39ff
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 15:00:36 +0100

    done

--- src/Algie.js
+++ src/Algie.js
@@ -4,6 +4,19 @@ function algie_42() {
 };
 
 function algie_array_with_same_values(array1, array2) {
-  // body...
+  if(_.isArray(array1) && _.isArray(array2)) {
+    if(_.isEqual(array1, array2)) {
+      return true
+    }
+    else if( _.isEmpty(_.xor(array1, array2))) {
+      return true
+    }
+    else if( !_.isEqual(array1, array2)) {
+      return false
+    }
+  } 
+  else {
+    return false
+  }
 }
 
