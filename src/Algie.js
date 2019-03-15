ef597393b9dccbdc3250abe3ffe977f210b5109a
Maxime Devallez <maxdeval02@gmail.com>
2019-03-15 16:29:27 +0100

    added conditions

--- src/Algie.js
+++ src/Algie.js
@@ -4,12 +4,23 @@ function algie_42() {
 };
 
 function algie_add2_dates(date1, date2) {
-  result = date1
-  year1 = date2.getFullYear()
-  year2 = date1.getFullYear()
-  finalYear = year1+year2
-  result.setFullYear(finalYear)
-  return result
+  if (_.isDate(date1) && _.isDate(date2) ){
+    day1 = date1.getDay()
+    day2 = date2.getDay()
+    month1 = date1.getMonth()
+    month2 = date2.getMonth()
+    if(day1 === day2 && month1 === month2){
+      result = date1
+      year1 = date2.getFullYear()
+      year2 = date1.getFullYear()
+      finalYear = year1+year2
+      result.setFullYear(finalYear)
+      return result      
+    }
+  }
+  else {
+    return false
+  }
 }
 
 algie_add2_dates(new Date('January 1, 2000 00:00:01'), new Date('January 1, 2000 00:00:01'))
