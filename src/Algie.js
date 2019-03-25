325c22ced8fb7d670878a2999dcccb852ffcbc16
Maxime Devallez <maxdeval02@gmail.com>
2019-03-25 09:28:31 +0100

    done

--- src/Algie.js
+++ src/Algie.js
@@ -4,6 +4,18 @@ function algie_42() {
 };
 
 
-function algie_string_weight(string) {
-  // body...
+function algie_string_weight(string){
+  var deburr_lower = _.deburr(_.lowerCase(string))
+  var alphabetic = deburr_lower.replace(/[^a-zA-Z\b]/g, "") 
+  var all = "abcdefghijklmnopqrstuvwxyz"
+  var splittedString = all.split("")
+  var object = {}
+  _.each(splittedString, function(value, index){ 
+      object[value] = index + 1
+  })
+  var alphabetic_detach = alphabetic.split("")
+ var numerics = _.map(alphabetic_detach, function(e) {return object[e]})
+  return _.reduce(numerics, function(sum, n) {
+    return sum + n;
+  }, 0);
 }
