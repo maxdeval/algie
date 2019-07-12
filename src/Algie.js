d012329bc57a1b48ede300e62ca884b17f4aca29
Maxime Devallez <maxdeval02@gmail.com>
2019-07-12 16:30:34 +0200

    trying random stuff

--- src/Algie.js
+++ src/Algie.js
@@ -1,3 +1,10 @@
 function algie_playWar(player1, player2) {
-  // body...
+
+  var numberOfCards = Math.floor((Math.random() * 10) + 1);
+  var player1Hands = []
+  var player1Hands = _.times(Math.floor((Math.random() * 10) + 1), numberOfCards)
+  console.log(player1Hands)
+
 }
+
+algie_playWar("bobby", "lilly")
