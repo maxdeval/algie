5c091fd0a06969c068756d405695110f79a1d41c
Maxime Devallez <maxdeval02@gmail.com>
2019-07-12 10:42:09 +0200

    cleaned

--- src/Algie.js
+++ src/Algie.js
@@ -3,7 +3,7 @@ function algie_fire_langage(sentence){
     res = []
     var splitted_sentence = ""
     splitted_sentence = _.split(sentence, "")
-    _.each(splitted_sentence, function(value, key){
+    _.each(splitted_sentence, function(value){
       res.push(value)
       if(value === "a" || value === "e"|| value === "i" || value === "o" || value === "u" || value === "y") {
         res.push("f")
