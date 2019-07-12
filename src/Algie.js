77ac00a219847635c2c828a66c3db5173c0b1d97
Maxime Devallez <maxdeval02@gmail.com>
2019-07-12 10:53:51 +0200

    reducing array

--- src/Algie.js
+++ src/Algie.js
@@ -1,6 +1,12 @@
 function algie_robust_array(array) {
   if(_.isArray(array)) {
     res = false
+    res = _.reduce(array, function(sum, n) {
+      return sum + n;
+    }, 0);
+    return res
   }
   return false
 }
+
+algie_robust_array([7, 3, 2])
