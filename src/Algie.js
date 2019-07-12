b786ebbf2ecdd2a9b82138e3f3aa0e709b8adf20
Maxime Devallez <maxdeval02@gmail.com>
2019-07-12 15:30:24 +0200

    counting exact match but taking in cout all incorrect match

--- src/Algie.js
+++ src/Algie.js
@@ -1,3 +1,24 @@
-function algie_actual_matches(argument) {
-  // body...
+function algie_actual_matches(array1, array2) {
+  var res = ""
+  if (_.isArray(array1) && _.isArray(array2)) {
+    if(array1.length != array2.length) {
+      return "size error"
+    }
+
+  if (_.isEmpty(_.xor(array1, array2))) {
+    return array1.length + " exact match and 0 incorrect match"
+  }
+
+    var counter = 0;
+    for(var i = 0; i < array1.length; i++) {
+      if(array1[i] === array2[i]) {
+        counter += 1
+      }
+  }
+
+  var incorrect = array1.length - _.toInteger(_.difference(array1, array2).length)
+     return counter + " exact match and " + incorrect + " incorrect match"
+  }
 }
+
+algie_actual_matches([1, 2, 2, 1, 0], [1, 3, 3, 4, 5])
