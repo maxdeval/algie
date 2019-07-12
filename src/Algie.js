86adf19ac9bab4ed74885bf31ccb3dcd91d302ac
Maxime Devallez <maxdeval02@gmail.com>
2019-07-12 10:44:18 +0200

    init algie_robust_array

--- src/Algie.js
+++ src/Algie.js
@@ -1,23 +1,3 @@
-function algie_ordinal_suffix(number){
-  if(_.isInteger(number)) {
-    var lastOne = number.toString().split('').pop();
-    console.log(lastOne)
-    if(lastOne === '1') {
-      return 'st'
-    }
-    if(lastOne === '2') {
-      return 'nd'
-    }
-    if(lastOne === '3') {
-      return 'rd'
-    }
-    if(lastOne === '4' || lastOne === '7' || lastOne === '9') {
-      return 'th'
-    }     
-  }
-  else {
-    return ''
-  }
+function algie_robust_array(argument) {
+  // body...
 }
-
-algie_ordinal_suffix(141)
