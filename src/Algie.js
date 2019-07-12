72ce020ad77f192a8808bf3b537aa9fc0d8e5273
Maxime Devallez <maxdeval02@gmail.com>
2019-07-12 10:35:53 +0200

    better way

--- src/Algie.js
+++ src/Algie.js
@@ -4,12 +4,19 @@ function algie_fire_langage(sentence){
   if(_.isString(sentence)){
     var splitted_sentence = ""
     splitted_sentence = _.split(sentence, "")
+    new_sentence = []
     _.each(splitted_sentence, function(value, key){
+      new_sentence.push(value)
       if(value === "a" || value === "e"|| value === "i" || value === "o" || value === "u" || value === "y") {
+        new_sentence.push("f")
+/*        temp = value
         splitted_sentence[key + 1] = "f"
+        console.log(temp)
+        splitted_sentence[key + 2] = temp
+        console.log(splitted_sentence)*/
       }
-      console.log(splitted_sentence)
     })
+    console.log(new_sentence)
     return res
   }
   return res
