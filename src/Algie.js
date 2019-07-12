c7dd32b1ae2210fad6a8f8d6c7543155bafce229
Maxime Devallez <maxdeval02@gmail.com>
2019-07-12 13:45:36 +0200

    just finding how pullAt works

--- src/Algie.js
+++ src/Algie.js
@@ -1,11 +1,17 @@
 function algie_robust_array(array) {
   if(_.isArray(array)) {
     res = false
+    temp1 = array
+    temp2 = 0
     _.each(array, function(value,key){
-      res = _.reduce(array, function(sum, key) { return sum + key; }, 0) 
+      temp2 = array[key]
+      _.pullAt(temp1, 0)
+      console.log(temp1)
+      res = _.reduce(temp1, function(sum, n) { return sum + n; }, 0) 
       console.log(res)
+      return res
     })
-    return res
+    
   }
   return false
 }
