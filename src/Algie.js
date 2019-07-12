8144fa44b5f3c361e383698437a94c16cf9a809a
Maxime Devallez <maxdeval02@gmail.com>
2019-07-12 09:36:34 +0200

    had to commit

--- src/Algie.js
+++ src/Algie.js
@@ -1,6 +1,23 @@
-function algie_42() {
-  // This already works, but this is more a basic example than something you would encounter on real projects.
-  return 42;
-};
+function algie_ordinal_suffix(number){
+  if(_.isInteger(number)) {
+    var lastOne = number.toString().split('').pop();
+    console.log(lastOne)
+    if(lastOne === '1') {
+      return 'st'
+    }
+    if(lastOne === '2') {
+      return 'nd'
+    }
+    if(lastOne === '3') {
+      return 'rd'
+    }
+    if(lastOne === '4' || lastOne === '7' || lastOne === '9') {
+      return 'th'
+    }     
+  }
+  else {
+    return ''
+  }
+}
 
-
+algie_ordinal_suffix(141)
