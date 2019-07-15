d2e69d42e885a55538c5ba178f48b5d4d09bd912
Maxime Devallez <maxdeval02@gmail.com>
2019-07-15 09:54:24 +0200

    found why couldn't fill

--- src/Algie.js
+++ src/Algie.js
@@ -3,7 +3,7 @@ function algie_playWar(player1, player2) {
   var numberOfCards = Math.floor((Math.random() * 10) + 1);
   var player1Hands = []
   var player1Hands = _.times(Math.floor((Math.random() * 10) + 1), numberOfCards)
-  console.log(_.times(Math.floor((Math.random() * 10) + 1), numberOfCards))
+  console.log(_.times(numberOfCards, _.constant(Math.floor((Math.random() * 10) + 1))))
   console.log(player1Hands)
 
 }
