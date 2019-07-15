4d24a5595878b0b6103c7d8d97ef739c03553527
Maxime Devallez <maxdeval02@gmail.com>
2019-07-15 11:02:15 +0200

    sorting in ascending for p1 and descending for p2

--- src/Algie.js
+++ src/Algie.js
@@ -8,8 +8,8 @@ function algie_playWar() {
 
   var length = prompt("chose the number of cards to have")
   var max = prompt("chose the max value you can have for a card")
-  var player1Hands = randomArray(_.toInteger(length), _.toInteger(max))
-  var player2Hands = randomArray(_.toInteger(length), _.toInteger(max))
+  var player1Hands = randomArray(_.toInteger(length), _.toInteger(max)).sort()
+  var player2Hands = randomArray(_.toInteger(length), _.toInteger(max)).sort(function(a, b){return b-a});
   console.log(player1Hands)
   console.log(player2Hands)
 
