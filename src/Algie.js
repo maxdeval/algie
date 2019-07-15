d290894274e3569ec22c54e6bda700e280dc045b
Maxime Devallez <maxdeval02@gmail.com>
2019-07-15 11:06:34 +0200

    init function to compare

--- src/Algie.js
+++ src/Algie.js
@@ -4,12 +4,17 @@ function randomArray(length, max) {
     });
 }
 
+function compareArray(array1, array2) {
+  // body...
+}
+
 function algie_playWar() {
 
   var length = prompt("chose the number of cards to have")
   var max = prompt("chose the max value you can have for a card")
   var player1Hands = randomArray(_.toInteger(length), _.toInteger(max)).sort()
   var player2Hands = randomArray(_.toInteger(length), _.toInteger(max)).sort(function(a, b){return b-a});
+
   console.log(player1Hands)
   console.log(player2Hands)
 
