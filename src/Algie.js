e0887873d4ef179683ed2c63e4f892383a200746
Maxime Devallez <maxdeval02@gmail.com>
2019-07-15 09:59:25 +0200

    cleaned

--- src/Algie.js
+++ src/Algie.js
@@ -7,9 +7,9 @@ function randomArray(length, max) {
 function algie_playWar(player1, player2) {
 
   var numberOfCards = Math.floor((Math.random() * 10) + 1);
-  var player1Hands = []
   var player1Hands = randomArray(3, 10)
   console.log(player1Hands)
+  
 }
 
 algie_playWar("bobby", "lilly")
