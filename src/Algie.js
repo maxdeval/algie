05a33fd0d3799a3470fc56e6cf13ad2ce3f5bb96
Maxime Devallez <maxdeval02@gmail.com>
2019-07-15 10:00:04 +0200

    2 hands players cards

--- src/Algie.js
+++ src/Algie.js
@@ -8,8 +8,10 @@ function algie_playWar(player1, player2) {
 
   var numberOfCards = Math.floor((Math.random() * 10) + 1);
   var player1Hands = randomArray(3, 10)
+  var player2Hands = randomArray(3, 10)
   console.log(player1Hands)
-  
+  console.log(player2Hands)
+
 }
 
 algie_playWar("bobby", "lilly")
