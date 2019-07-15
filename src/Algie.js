c035f9a22c3b37b1dc70406100c9ce9b8a53573d
Maxime Devallez <maxdeval02@gmail.com>
2019-07-15 09:58:41 +0200

    betterr way

--- src/Algie.js
+++ src/Algie.js
@@ -1,11 +1,15 @@
+function randomArray(length, max) {
+    return Array.apply(null, Array(length)).map(function() {
+        return Math.round(Math.random() * max);
+    });
+}
+
 function algie_playWar(player1, player2) {
 
   var numberOfCards = Math.floor((Math.random() * 10) + 1);
   var player1Hands = []
-  var player1Hands = _.times(Math.floor((Math.random() * 10) + 1), numberOfCards)
-  console.log(_.times(numberOfCards, _.constant(Math.floor((Math.random() * 10) + 1))))
+  var player1Hands = randomArray(3, 10)
   console.log(player1Hands)
-
 }
 
 algie_playWar("bobby", "lilly")
