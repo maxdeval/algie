72cce257ccc9e1a0d3f7daf2d8966ef246454925
Maxime Devallez <maxdeval02@gmail.com>
2019-07-18 10:48:44 +0200

    working for row but very cumbersome

--- src/Algie.js
+++ src/Algie.js
@@ -1,12 +1,31 @@
 function algie_connect4(row1, row2, row3, row4, row5, row6) {
 
-  console.log(row3[0])
+  var counter = 0
 
+  for (var i = 0; i < 5; i++) {
+    console.log(counter)
+    console.log(row1[i])
+    console.log(row1[i+1])
+    if (row1[i] === row1[i+1]) {
+      console.log("dans le if")
+      counter = counter + 1
+    }
+    if (counter === 4) {
+      return true
+    }
+    if (row1[i] != row1[i+1]) {
+          console.log("dans le else")
+          counter = 0
+    }
+    
+  }
+
+  return false
 
 }
 
 algie_connect4(
-        [" ", " ", " ", " ", " ", " "],
+        ["x", "x", "x", "x", "x", " "],
         [" ", " ", "o", "x", "x", " "],
         ["x", "o", "x", "x", "x", "o"],
         ["o", "x", "o", "o", "x", "o"],
