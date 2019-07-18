042a0f2caeeb06c2c705b8726d19f06b3e2e87c7
Maxime Devallez <maxdeval02@gmail.com>
2019-07-18 11:32:37 +0200

    win per row

--- src/Algie.js
+++ src/Algie.js
@@ -7,6 +7,18 @@ function algie_connect4(row1, row2, row3, row4, row5, row6) {
 
   var counter = 0
 
+  for (var i = 0; i<winCombination.length; i++) {
+    var count = 0
+    for (var j = 0; j<winCombination.length; j++) {
+      if(board[winCombination[i][j]] === coin) {
+        count ++
+      }
+      if (count === 3) {
+        throw 'player ' + coin + ' won'
+      }
+    }
+  }
+
 /*  for (var i = 0; i < 5; i++) {
     if (row1[i] === row1[i+1]) {
       counter = counter + 1
@@ -80,6 +92,7 @@ function algie_connect4(row1, row2, row3, row4, row5, row6) {
   }*/
 
 
+  var winCombination = [[0,1,2,3], [1,2,3,4], [2,3,4,5], [6,7,8,9], [7,8,9,10], [8,9,10,11], [12,13,14,15], [13,14,15,16], [17,18,19,20],[18,19,20,21], [22,23,24,25], [23,24,25,26], [24,25,26,27], [25,26,27,28], [26,27,28,29], [30,31,32,33], [31,32,33,34], [32,33,34,35]]
 
   return false
 
@@ -92,3 +105,5 @@ algie_connect4(
         ["o", "x", "o", "x", "o", "x"],
         ["x", "o", "x", "o", "x", "o"],
         )
+
+
