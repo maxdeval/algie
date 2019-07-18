be4c484d7ed409679d514ef648e0fd651975ce9f
Maxime Devallez <maxdeval02@gmail.com>
2019-07-18 10:54:13 +0200

    comparing all rows

--- src/Algie.js
+++ src/Algie.js
@@ -3,23 +3,78 @@ function algie_connect4(row1, row2, row3, row4, row5, row6) {
   var counter = 0
 
   for (var i = 0; i < 5; i++) {
-    console.log(counter)
-    console.log(row1[i])
-    console.log(row1[i+1])
     if (row1[i] === row1[i+1]) {
-      console.log("dans le if")
       counter = counter + 1
     }
     if (counter === 4) {
       return true
     }
     if (row1[i] != row1[i+1]) {
-          console.log("dans le else")
           counter = 0
     }
-    
+
+  for (i = 0; i < 5; i++) {
+    if (row2[i] === row2[i+1]) {
+      counter = counter + 1
+    }
+    if (counter === 4) {
+      return true
+    }
+    if (row2[i] != row2[i+1]) {
+          counter = 0
+    }
   }
 
+  for (i = 0; i < 5; i++) {
+    if (row3[i] === row3[i+1]) {
+      counter = counter + 1
+    }
+    if (counter === 4) {
+      return true
+    }
+    if (row1[i] != row1[i+1]) {
+          counter = 0
+    }
+  }
+
+  for (i = 0; i < 5; i++) {
+    if (row4[i] === row4[i+1]) {
+      counter = counter + 1
+    }
+    if (counter === 4) {
+      return true
+    }
+    if (row1[i] != row1[i+1]) {
+          counter = 0
+    }
+  }
+
+  for (i = 0; i < 5; i++) {
+    if (row5[i] === row6[i+1]) {
+      counter = counter + 1
+    }
+    if (counter === 4) {
+      return true
+    }
+    if (row1[i] != row1[i+1]) {
+          counter = 0
+    }
+  }
+
+  for (i = 0; i < 5; i++) {
+    if (row6[i] === row6[i+1]) {
+      counter = counter + 1
+    }
+    if (counter === 4) {
+      return true
+    }
+    if (row6[i] != row6[i+1]) {
+          counter = 0
+    }
+  }
+
+
+  
   return false
 
 }
